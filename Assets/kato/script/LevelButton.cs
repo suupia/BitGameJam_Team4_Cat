ea6219aa0bd1ce412f3305a6d26e1086abd321bb
SHAULA\tsuba <tsubasa.kato.2000@gmail.com>
2023-06-27 14:46:07 +0900

    sample

---
+++ Assets/kato/script/LevelButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+    public int levelIndex;
+    private Button btn;
+
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.interactable = levelIndex <= ProgressManager.GetCompletedLevelIndex() + 1;
+        btn.onClick.AddListener(OnButtonClicked);
+    }
+
+    void OnButtonClicked()
+    {
+        ProgressManager.CompleteLevelIndex(levelIndex);
+        SceneManager.LoadScene("testStage");
+    }
+}
---
+++ Assets/kato/script/ProgressManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class ProgressManager
+{
+    private const string KeyActiveLevelIndex = "ActiveLevelIndex";
+
+    public static int GetCompletedLevelIndex()
+    {
+        return PlayerPrefs.GetInt(KeyActiveLevelIndex, 0);
+    }
+
+    public static void CompleteLevelIndex(int levelIndex)
+    {
+        int currentIndex = GetCompletedLevelIndex();
+        if (levelIndex > currentIndex)
+        {
+            PlayerPrefs.SetInt(KeyActiveLevelIndex, levelIndex);
+            PlayerPrefs.Save();
+        }
+    }
+}

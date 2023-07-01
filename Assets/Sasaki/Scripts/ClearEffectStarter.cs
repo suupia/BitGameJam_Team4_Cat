a92ff7350f47f9550cf81d9225a6ed235957be08
halset09 <103188922+halset09@users.noreply.github.com>
2023-07-01 17:33:58 +0900

    クリア時の文字表示の追加

--- Assets/Sasaki/Scripts/ClearEffectStarter.cs
+++ Assets/Sasaki/Scripts/ClearEffectStarter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [System.Serializable]  // このクラスがUnityのインスペクタで編集可能になります
 public class TimedAudioSource
@@ -16,23 +17,51 @@ public class TimedParticleSystem
     public float startTime;
 }
 
+[System.Serializable]
+public class TimedTextMeshProUGUI
+{
+    public TextMeshProUGUI textMeshProUGUI;
+    public float startTime;
+    public float endTime;
+}
+
 public class ClearEffectStarter : MonoBehaviour
 {
     public List<TimedAudioSource> timedAudioSources;
     public List<TimedParticleSystem> timedParticleSystems;
+    public List<TimedTextMeshProUGUI> timedTexts;
+
+
+    void Start() 
+    {
+        // Initialization
+        foreach (var timedText in timedTexts)
+        {
+            timedText.textMeshProUGUI.enabled = false;
+        }
+    }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            foreach (var timedAudioSource in timedAudioSources)
-            {
-                StartCoroutine(StartAfterDelay(timedAudioSource.audioSource.Play, timedAudioSource.startTime));
-            }
-            foreach (var timedParticleSystem in timedParticleSystems)
-            {
-                StartCoroutine(StartAfterDelay(timedParticleSystem.particleSystem.Play, timedParticleSystem.startTime));
-            }
+            StartEffect();
+        }
+    }
+
+    public void StartEffect()
+    {
+        foreach (var timedAudioSource in timedAudioSources)
+        {
+            StartCoroutine(StartAfterDelay(timedAudioSource.audioSource.Play, timedAudioSource.startTime));
+        }
+        foreach (var timedParticleSystem in timedParticleSystems)
+        {
+            StartCoroutine(StartAfterDelay(timedParticleSystem.particleSystem.Play, timedParticleSystem.startTime));
+        }
+        foreach (var timedText in timedTexts)
+        {
+            StartCoroutine(ShowAndHideText(timedText.textMeshProUGUI, timedText.startTime, timedText.endTime));
         }
     }
 
@@ -40,5 +69,13 @@ public class ClearEffectStarter : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         action();
+    }
+
+    IEnumerator ShowAndHideText(TextMeshProUGUI textMeshProUGUI, float startTime, float endTime)
+    {
+        yield return new WaitForSeconds(startTime);
+        textMeshProUGUI.enabled = true;
+        yield return new WaitForSeconds(endTime - startTime);
+        textMeshProUGUI.enabled = false;
     }
 }

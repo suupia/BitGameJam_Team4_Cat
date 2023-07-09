3bb17196622d4e3335e6fa75aa6db1462e786af1
suupia <morita777k@icloud.com>
2023-07-09 18:10:10 +0900

    SelectStageButtonを作成し、GridLayoutGroupでボタンを管理

--- Assets/kato/script/LevelButton.cs
+++ Assets/kato/script/LevelButton.cs
@@ -7,10 +7,10 @@ using System.Collections.Generic;
 public class LevelButton : MonoBehaviour
 {
     public int levelIndex;
-    private Button btn;
-    private Image img; // 新たに追加
-    private static List<LevelButton> allLevelButtons = new List<LevelButton>();
     public SceneName sceneToLoad;
+    [SerializeField] Image img; // 新たに追加
+    private static List<LevelButton> allLevelButtons = new List<LevelButton>();
+    private Button btn;
 
     void Awake()
     {
@@ -27,7 +27,6 @@ public class LevelButton : MonoBehaviour
     void Start()
     {
         btn = GetComponent<Button>();
-        img = GetComponent<Image>(); // 新たに追加
         RefreshButtonInteractableState();
         btn.onClick.AddListener(OnButtonClicked);
     }
@@ -44,6 +43,7 @@ public class LevelButton : MonoBehaviour
 
         // もしButtonが押せる(isButtonInteractable == true)なら透明度を1（全く透明でない）に、そうでないなら透明度を0.5（半透明）に設定
         img.color = new Color(img.color.r, img.color.g, img.color.b, isButtonInteractable ? 1f : 0.5f);
+        Debug.Log($"img = {img}");
     }
 
     public static void RefreshAllButtons()
